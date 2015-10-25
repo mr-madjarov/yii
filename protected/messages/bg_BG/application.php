0457f9e134f5bfc10f0e44776089ca04549eb1e9
Madzharov <yusm@abv.bg>
2015-10-25 03:31:18 +0300

    First Commit  push all

--- protected/messages/bg_BG/application.php
+++ protected/messages/bg_BG/application.php
@@ -8,11 +8,4 @@
 return array(
     'Home' => 'Начало',
     'Login' => 'Вход',
-    'Number' => 'Число',
-    'Result' => 'Резултат',
-    'The number was: {number}' => 'Числото беше: {number}',
-    'Cows: {cows}, Bulls: {bulls}' => 'Крави: {cows}, Бикове: {bulls}',
-    'Congratulations! You have found the number!' => 'Поздравления! Вие познахте числото!',
-    'You have surrendered!' => 'Вие се предадохте!',
-    'Guess' => 'Познай',
 );
