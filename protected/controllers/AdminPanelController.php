5279cd18e3da03ab579d89f25894d2b98e4cb290
Madzharov <yusm@abv.bg>
2015-10-25 03:34:00 +0300

    First Commit  push all

--- protected/controllers/AdminPanelController.php
+++ protected/controllers/AdminPanelController.php
@@ -14,7 +14,8 @@ class AdminPanelController extends Controller
     public function filters()
     {
         return array(
-            'accessControl', // perform access control for CRUD operations
+            'accessControl',
+            // perform access control for CRUD operations
         );
     }
 
